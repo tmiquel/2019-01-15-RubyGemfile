3e3d33627764d05a012a1a6561159df53514a668
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-01-16 11:25:01 +0100

    Rubocop done

--- words_counter/lib/word_counter.rb
+++ words_counter/lib/word_counter.rb
@@ -1,68 +1,69 @@
 #! usr/env/bin ruby
-dictionnary = ["the", "of", "and", "to", "a", "in", "for", "is", "on", "that", "by", "this", "with", "i", "you", "it", "not", "or", "be", "are"]
+# frozen_string_literal: true
+
+dictionnary = %w[the of and to a in for is on that by this with i\
+you it not or be are]
 
 def words_counter(corpus_string, dict_array)
-	words = corpus_string.downcase.split(/\W+/)
+  words = corpus_string.downcase.split(/\W+/)
 
-	words_count_hash = Hash.new { |hash, word_key| hash[word_key] = 
-		words.count(word_key)}
+  words_count_hash = Hash.new do |hash, word_key|
+    hash[word_key] = words.count(word_key)
+  end
 
-	dict_array.each	do |searched_word|
-		words_count_hash[searched_word]
-		words_count_hash.keep_if  {|word_key, count_value| !count_value.zero?}
-	end
+  dict_array.each do |searched_word|
+    words_count_hash[searched_word]
+    words_count_hash.keep_if { |_word_key, count_value| !count_value.zero? }
+  end
 
-	puts words_count_hash
+  puts words_count_hash
 
-	return words_count_hash
+  words_count_hash
 end
 
-puts "="*60
-puts "NUMBER OF WORD IN SHAKESPEARE CORPUS"
-puts "="*60
+puts '=' * 60
+puts 'NUMBER OF WORD IN SHAKESPEARE CORPUS'
+puts '=' * 60
 puts
-puts "DICTIONARY"
-puts dictionnary.join(" ").inspect
+puts 'DICTIONARY'
+puts dictionnary.join(' ').inspect
 puts
-words_counter(IO.read("shakespeare.txt"), dictionnary)
+words_counter(IO.read('shakespeare.txt'), dictionnary)
 puts
 
-insults_array = IO.read("insults.txt").downcase.split(/\W+/)
+insults_array = IO.read('insults.txt').downcase.split(/\W+/)
 
-puts 
-puts "="*60
-puts "NUMBER OF INSULTS IN SHAKESPEARE CORPUS"
-puts "="*60
 puts
-puts "DICTIONARY"
-puts insults_array.join(" ").inspect
+puts '=' * 60
+puts 'NUMBER OF INSULTS IN SHAKESPEARE CORPUS'
+puts '=' * 60
 puts
-words_counter(IO.read("shakespeare.txt"), insults_array)
+puts 'DICTIONARY'
+puts insults_array.join(' ').inspect
 puts
-puts "END"
-puts 
-
-
-
+words_counter(IO.read('shakespeare.txt'), insults_array)
+puts
+puts 'END'
+puts
 
 # def perform
 
-# puts dictionary = ["below", "down", "go", 
-# 	"going", "horn", "how", "howdy", "it", "i", 
-# 	"low", "own", "part", "partner", "sit"]
+# puts dictionary = ["below", "down", "go",
+#   "going", "horn", "how", "howdy", "it", "i",
+#   "low", "own", "part", "partner", "sit"]
 
-# # puts "new sentence : below"	
+# # puts "new sentence : below"
 # # puts word_counter("below", dictionary)
 # # => {"below"=>1, "low"=>1}
 
 # # puts "="*60
 # puts "new sentence : Howdy partner, sit down! How's it going?"
-# puts word_counter("Howdy partner, sit down! How's it going?", 
-# 	dictionary)
+# puts word_counter("Howdy partner, sit down! How's it going?",
+#   dictionary)
 
 # end
 
 # perform
-# => {"down"=>1, "how"=>2, "howdy"=>1,"go"=>1, 
-# 	"going"=>1, "it"=>2, "i"=> 3, "own"=>1,"part"=>1,
-# 	"partner"=>1,"sit"=>1}
+# => {"down"=>1, "how"=>2, "howdy"=>1,"go"=>1,
+#   "going"=>1, "it"=>2, "i"=> 3, "own"=>1,"part"=>1,
+#   "partner"=>1,"sit"=>1}
